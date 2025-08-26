ba21c69778b94e2343017b80e19ad1a32b3d3534
Ana Beatriz Silva Santos <ana1908beatrizsantos@gmail.com>
2025-08-26 10:49:08 -0300

    Enviando tudo

---
+++ Full Stack/controllers/user_controller.py
@@ -0,0 +1,24 @@
+from flask import render_template, request, redirect, url_for
+from models.user import User, db
+
+class UserController:
+    @staticmethod
+    def index():
+        users = User.query.all()
+        return render_template('index.html', users=users)
+
+    @staticmethod
+    def contact():
+        if request.method == 'POST':
+            name = request.form['name']
+            email = request.form['email']
+
+            # validações simples: se usuário já existe no db, etc
+
+            new_user = User(name=name, email=email)
+            db.session.add(new_user)
+            db.session.commit()
+
+            return redirect(url_for('index'))
+
+        return render_template('contact.html')
---
+++ Full Stack/models/user.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# Cada classe que herda de db.Model se torna uma tabela no banco de dados.
+class User(db.Model): # Declaração de um novo modelo/tabela: User
+    id = db.Column(db.Integer, primary_key=True) # Define uma coluna ... 
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+
+    __tablename__ = 'users'
